5a2d7220f7b6b3ca40b1d155fb73302d081ba16a
Aman <97949698+amwn1@users.noreply.github.com>
2024-03-09 12:21:20 +0000

    dataTypes

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,15 @@
-console.log("Aman")
+const accountId = 12223 
+let accuntEmail = "aman@gmail.com"
+var password = 1234
+accountCity = "Jalandhar" // a variable can be declared this as well , just not the orthodox way and not  a good practice .
+
+let state ; // the variable is just declared , so the value is just "UNDEFINED";
+
+// we use let because , of the block scope as it used to be an issue in the primitive js , PREFER NOT TO USE VAR ;
+
+// accountId = 23
+accuntEmail = "some.gmail.com"
+password = 2222
+accountCity = "notJalandhar"
+// console.log(accountId);
+console.table([accountId,accuntEmail,accountCity,password,state])
---
+++ 02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" // treats all the JS code as the newer version
+
+// alert(3+3) // won't work as we using node.js and not browser
+
+// console.log(3+3) console.log("Aman")  this wont work you need to use the semicolon between them , though the code readablity is the worst ; So avoid the practice\
+
+// console.log(3+3)  
+// console.log("Aman") // this is the way 
+
+let name = "Aman";
+let age = 20;
+let isLoggedin = false 
+
+// let can be used like this 
+// there is a differnece between null and undefined , if a value is null then it is not undefined but empty , vice verca;
+// symbol is also a data type mostly used for uniqueness 
+
+// OBJECT 
+
+console.log(typeof null); // the output results to "object"\
+console.log(typeof undefined); // the output results to "undefined"
