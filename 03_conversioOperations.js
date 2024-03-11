70f3e091018b6c47e01c749c11acc0eb5469e55c
Aman <97949698+amwn1@users.noreply.github.com>
2024-03-11 10:47:50 +0000

    type_conversion

---
+++ 03_conversioOperations.js
@@ -0,0 +1,33 @@
+// let score = 33 
+
+// console.log(typeof score); // number 
+// console.log(typeof(score)); // number
+
+// let scor = "33"
+// let s = "44asd" 
+
+// console.log(typeof scor); // string
+// console.log(typeof(scor)); // string
+
+// now
+
+// let valueInNumber = Number(scor) ; // this will convert the string to number
+// let value = Number(s)
+// console.log(typeof valueInNumber); // op number
+// console.log(typeof valueInNumber); // op number
+// console.log(valueInNumber) // op 33
+// console.log(value) // op NaN (Not a number)
+
+// let n = null // will give you 0 
+// let u = undefined // will give you NaN
+// let b = true // will give you 1 for true and 0 for false 
+// let str = "Aman" // will give you NaN (obviously)
+
+let n = 1 
+let booleanIsLoggedIn = Boolean(n)
+console.log( booleanIsLoggedIn); // true 
+// 1 => true , 0 => false 
+// "" => false 
+// "Aman" => true 
+
+
