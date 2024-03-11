c076e3b73ee37b38df7b43f87013b339b4f82b83
Aman <97949698+amwn1@users.noreply.github.com>
2024-03-11 11:16:08 +0000

    operations

--- 03_conversioOperations.js
+++ 03_conversioOperations.js
@@ -23,17 +23,48 @@
 // let b = true // will give you 1 for true and 0 for false 
 // let str = "Aman" // will give you NaN (obviously)
 
-let n = 1 
-let booleanIsLoggedIn = Boolean(n)
-console.log( booleanIsLoggedIn); // true 
-// 1 => true , 0 => false 
-// "" => false 
-// "Aman" => true 
-// now to talk about strings
+// let n = 1 
+// let booleanIsLoggedIn = Boolean(n)
+// console.log( booleanIsLoggedIn); // true 
+// // 1 => true , 0 => false 
+// // "" => false 
+// // "Aman" => true 
+// // now to talk about strings
 
-let someNumber =33 
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber =33 
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
 
+//  ********************* OPERATIONS **************** 
 
+let value = 3
+let negValue = -value
+
+// console.log(negValue); will give you -3
+// console.log(2+2);
+// 2-2
+// 2*2
+// 2/3
+// 2%3 
+// 2**2 // this is for the power
+
+// let str1 = "Hello"
+// let str2 = " Aman"
+// let str3 = str1 + str2 ;
+// console.log(str3); // Will give you Hello Aman
+
+// console.log("1" + 2); // will give you 12 
+// console.log("1" + 2 + 2) //  NOTE: WIll give you 122
+// console.log(1+2+"2"); will give you 32
+
+// console.log(true) // will gve you true
+// console.log(+true) // will give you 1 
+
+let counter = 100
+counter++;
+console.log(counter)
+++counter;
+console.log(counter)
+
+//prefix and postfix
