5b3c3a3791d2549baca29708ee10e53fb8adcb47
Aman <97949698+amwn1@users.noreply.github.com>
2024-03-14 14:55:36 +0000

    Some Methods and prototype

--- 05_strings.js
+++ 05_strings.js
@@ -1,11 +1,23 @@
-const name = "Aman" 
-const age =  22
+// const name = "Aman" 
+// const age =  22
 
 // console.log(name + repoCOunt + "ka nahi hai");
 
-console.log(`Hello my name is ${name} and my age is ${age}`); // This is the modern way and is called string interopolation
+// console.log(`Hello my name is ${name} and my age is ${age}`); // This is the modern way and is called string interopolation
 
 const gameName = new String("Baldurs Gate") // one way to declare a string .
 
-console.log(gameName[7])
-console.log(gameName[7])
+// console.log(gameName[9])
+// console.log(gameName.__proto__);
+// console.log(gameName.toUpperCase()); // to uppercase
+// console.log(gameName.charAt(2)); // will locate the character at the given location 
+// console.log(gameName.indexOf('t')); // will locate where the character is located
+
+// const newString = gameName.substring(0,4)
+// console.log(newString) // Bald so 0 , 1, 2, 3 char are included only and not at the 4th index
+
+// const anotherString = gameName.slice(-8,4) // -8 will resukt the string starting from reverse
+// console.log(anotherString); Bald // substring will ignore the valuses
+
+const newStrongOne = " aman "
+console.log(newStrongOne.trim()); // all the whitespaces are trimmed :) 
