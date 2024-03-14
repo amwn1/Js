f972edd5be3c1c21753d63d7830b9d421d4a0906
Aman <97949698+amwn1@users.noreply.github.com>
2024-03-14 13:02:22 +0000

    String

---
+++ 05_strings.js
@@ -0,0 +1,11 @@
+const name = "Aman" 
+const age =  22
+
+// console.log(name + repoCOunt + "ka nahi hai");
+
+console.log(`Hello my name is ${name} and my age is ${age}`); // This is the modern way and is called string interopolation
+
+const gameName = new String("Baldurs Gate") // one way to declare a string .
+
+console.log(gameName[7])
+console.log(gameName[7])
