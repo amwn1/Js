2127e6bdd5ddfc94391a3b700cc848fb66782da8
Aman <97949698+amwn1@users.noreply.github.com>
2024-03-31 14:09:14 +0000

    starting controlFlow

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,26 @@
+// if condition , within the parameter : the code in scope will run only if the condition in the parameter is TRUE
+
+// if(condition){
+
+// }
+
+// > , < , <= ,>= , == , != : Some of the conditional operators 
+
+// NOTE : There is a differnence between == or ===(strict equal) this also checks data type as well  
+
+// now there is an if else statement
+// let a = 8 ;
+// if(a <10){
+//    console.log("less than 10");
+// }
+// else{
+//     console.log("More than 10");
+// }
+
+// OP : Less than 10
+
+// if the logic in the code is of one line then implicit scope is applied 
+
+let a = 8 ;
+if(a <10) console.log("test") ; // will still work 
+
