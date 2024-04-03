4ba70db09b81261e5b5200a2c6fa5fa1291eda9a
Aman <97949698+amwn1@users.noreply.github.com>
2024-04-03 07:20:18 +0000

    forLoops

---
+++ 05_iterations/one.js
@@ -0,0 +1,31 @@
+// // for loop
+// for(let i = 0 ; i<10 ; i++){
+    // console.log(i);
+// }
+// // 0 - 9 the output will be printed
+// // The elements aren't accessible outside the scope . (Which is nice)
+
+// // you can have nested loops as well 
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(`Outer loop ${i}`)
+//     for (let j = 0; j < 2 ; j++) { //don't take I as that i is already in the scope
+//         console.log(j)
+//     }
+    
+// }
+
+//keywords 
+
+//sometimes you can break a loop midaway as well ,
+// :break : it will break the for loop if its condition is reached 
+// :continue : if the condition is assigned to it the loop will carry on nonetheless
+
+// for(let i = 0 ; i<7 ; i++){
+//   if(index ==  5){
+//     break ; this will stop the execution of the loop
+//   }
+
+// }
+
+// vice veraca for continue as well
