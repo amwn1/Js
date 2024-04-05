cc65a47b33b81f23be2805361cf30b68233404bd
Aman <97949698+amwn1@users.noreply.github.com>
2024-04-05 14:05:10 +0000

    filter

---
+++ 05_iterations/six.js
@@ -0,0 +1,40 @@
+// const coding  = ["js","ruby","java","python","cpp"]
+
+// const values  = coding.forEach((item) => {
+//     console.log(item);
+//     return item        //for each wont return anything 
+// })
+
+// console.log(values); // undefined 
+
+// const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// //
+// const newNums =  myNums.filter( (num) =>  num > 4)    // OP :[ 5, 6, 7, 8, 9, 10 ]
+// console.log(newNums)
+
+// const nums =  myNums.filter( (num) =>  {
+//     num > 4 ;   // if the codes ends here then it won return anythin
+//     // return num > 4 ;  // this will work 
+// })   
+// console.log(newNums)
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  const userBooks = books.filter((bk) => bk.genre === "History" ) // this works as filter is applied 
+//   console.log(userBooks);
+
+ // now do the books who are published after 2004
+
+ const newBooks = books.filter((bk) => bk.publish >= 2004)
+ console.log(newBooks); // you can do so many filter 
